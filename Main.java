4af0b3fa4757412ff0384f254121587d4b95ea02
Stusowski <126696159+Stusowski@users.noreply.github.com>
2023-03-29 13:11:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    for(int j=1; j<11; j++){
+      for(int i=0; i<10; i++){
+        int k=(i+1)*j;
+        System.out.print("  "+k);
+      }
+      System.out.println();
+    }
+  }
+}
